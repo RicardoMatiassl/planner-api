18ac56d4f8fcf6ffd7166c273f06753c6c319e89
Ricardo Matias <ricardo.matias@ufape.edu.br>
2026-07-17 10:51:16 -0300

    fix(api): restaura rotas de metas, lembretes e categorias

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,21 @@
 <?php
 
+use App\Http\Controllers\Api\CategoriaController;
+use App\Http\Controllers\Api\LembreteController;
+use App\Http\Controllers\Api\MetaController;
 use App\Http\Controllers\Api\TarefaController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\Api\CategoriaController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\LembreteController;
 
 Route::post('/registrar', [AuthController::class, 'registrar']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/perfil', [AuthController::class, 'meuPerfil']);
 
+    Route::apiResource('categorias', CategoriaController::class);
+    Route::apiResource('metas', MetaController::class);
     Route::apiResource('tarefas', TarefaController::class);
+    Route::apiResource('lembretes', LembreteController::class);
 });
